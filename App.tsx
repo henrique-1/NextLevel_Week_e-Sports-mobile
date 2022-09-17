a728122768b6de3237546048348b269743c5d799
Henrique Bissoli Malaman Alonso <bissoli@3stamina.com>
2022-09-17 03:20:36 -0300

    Aula 05

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react'
 import { StatusBar } from "react-native";
 import { Background } from "./src/components/Background";
 import {
@@ -7,8 +8,12 @@ import {
   Inter_700Bold,
   Inter_900Black
 } from "@expo-google-fonts/inter";
+import { Subscription } from 'expo-modules-core'
+import * as Notifications from 'expo-notifications';
 import { Routes } from './src/routes';
 import { Loading } from "./src/components/Loading";
+import './src/services/notificationConfigs';
+import { getPushNotificationToken } from './src/services/getPushNotificationToken';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,6 +22,30 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  })
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification);
+    });
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(notification => {
+      console.log(notification);
+    })
+
+    return () => {
+      if (getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current)
+        Notifications.removeNotificationSubscription(responseNotificationListener.current)
+      }
+    }
+  }, [])
 
   return (
     <Background>
--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -10,16 +10,23 @@ import { THEME } from '../../theme';
 import { GameParams } from '../../@types/navigation';
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { DuoCard, DuoCardProps } from '../../components/DuoCard';
+import { DuoMatch } from '../../components/DuoMatch';
 
 export function Game() {
-
     const [duos, setDuos] = useState<DuoCardProps[]>([]);
+    const [discordDuoSelected, setDiscordDuoSelected] = useState('');
     const navigation = useNavigation();
     const route = useRoute();
     const game = route.params as GameParams;
 
     function handleGoBack() {
         navigation.goBack();
+    }
+
+    async function getDiscordUser(adsId: string) {
+        fetch(`http://192.168.15.3:3333/ads/${adsId}/discord`)
+            .then(response => response.json())
+            .then(data => setDiscordDuoSelected(data.discord));
     }
 
     useEffect(() => {
@@ -53,7 +60,7 @@ export function Game() {
                     keyExtractor={item => item.id}
                     renderItem={({ item }) => (
                         <DuoCard data={item}
-                            onConnect={() => { }} />
+                            onConnect={() => { getDiscordUser(item.id) }} />
                     )}
                     horizontal
                     style={styles.containerList}
@@ -65,6 +72,12 @@ export function Game() {
                         </Text>
                     )}>
                 </FlatList>
+
+                <DuoMatch
+                    visible={discordDuoSelected.length > 0}
+                    discord={discordDuoSelected}
+                    onClose={() => setDiscordDuoSelected('')}
+                />
 
             </SafeAreaView>
         </Background>
